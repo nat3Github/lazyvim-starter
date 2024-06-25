0c91807f265d4cfe9ca9163827d86b67478a3447
nat3 <billing.nate@gmx.de>
2024-06-25 15:16:48 +0200

    k

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,8 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if false then return {
-  -- add any tools you want to have installed below
-} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
